e44612b4dd5631e41d87bc2295dcf8c7726fd823
Alec Patterson <8888alec@gmail.com>
2024-05-20 17:37:48 -0400

    routes

--- client/src/router/routes.js
+++ client/src/router/routes.js
@@ -5,11 +5,11 @@ export default [
 
     children: [
       { path: '', component: () => import('pages/homePage.vue'), meta: { title: 'Home' } },
-      { path: 'queryOne', component: () => import('pages/queryOne.vue'), meta: { title: 'Runs-to-Outs by Salary range' } },
-      { path: 'queryTwo', component: () => import('pages/queryTwo.vue'), meta: { title: 'Team Spending Per Win' } },
-      { path: 'queryThree', component: () => import('pages/queryThree.vue'), meta: { title: 'Foreign-born MLB player distributions' } },
-      { path: 'queryFour', component: () => import('pages/queryFour.vue'), meta: { title: 'Player Height & Performance' } },
-      { path: 'queryFive', component: () => import('pages/queryFive.vue'), meta: { title: 'Home Runs & Post Season' } },
+      { path: 'runsToOuts', component: () => import('pages/queryOne.vue'), meta: { title: 'Runs-to-Outs by Salary range' } },
+      { path: 'spendingPerWin', component: () => import('pages/queryTwo.vue'), meta: { title: 'Team Spending Per Win' } },
+      { path: 'foreignBorn', component: () => import('pages/queryThree.vue'), meta: { title: 'Foreign-born MLB player distributions' } },
+      { path: 'sizePerformance', component: () => import('pages/queryFour.vue'), meta: { title: 'Player Height & Performance' } },
+      { path: 'hrPost', component: () => import('pages/queryFive.vue'), meta: { title: 'Home Runs & Post Season' } },
       { path: 'feedback', component: () => import('pages/submitFeedback.vue'), meta: { title: 'Feedback' } },
       { path: 'thanks', component: () => import('pages/thankYou.vue'), meta: { title: 'Thank You' } },
     ]
